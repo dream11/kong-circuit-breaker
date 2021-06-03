76c75b6283a960c4c4184864e73bee743d453573
Anuj Gupta <anuj@dream11.com>
2021-06-03 18:09:47 +0530

    fix errors in ci

--- .ci/scripts/run-tests.sh
+++ .ci/scripts/run-tests.sh
@@ -5,5 +5,5 @@ export GOJIRA_USE_SNAPSHOT=1
 export GOJIRA_TAG=2.2.0
 ./gojira/gojira.sh up --egg gojira-compose.yaml --git-https  
 sleep 5s
-gojira run "cd /kong/custom_plugins/circuit-breaker && luarocks make"
-gojira run "bin/busted /kong/custom_specs/plugins/circuit-breaker/01-access_spec.lua -o custom_specs/output-handlers/custom_format.lua"
+./gojira/gojira.sh run "cd /kong/custom_plugins/circuit-breaker && luarocks make"
+./gojira/gojira.sh run "bin/busted /kong/custom_specs/plugins/circuit-breaker/01-access_spec.lua -o custom_specs/output-handlers/custom_format.lua"
